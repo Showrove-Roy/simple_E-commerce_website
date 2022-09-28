96c03bfce9a162a5242753180c08cc988bc86211
Showrove Roy <showrove200@gmail.com>
2022-09-28 17:07:59 +0600

    Product card added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Product.css";
+
+const Product = (props) => {
+  const { name, price, img, seller, ratings } = props.product;
+  return (
+    <div className='product-cart'>
+      <img className='product-img' src={img} alt='' />
+      <div className='cart-body'>
+        <h6 className='product-name'>{name}</h6>
+        <p className='price'>Price : ${price}</p>
+        <p>Manufacturer : {seller}</p>
+        <p>Rating : {ratings}</p>
+      </div>
+      <button className='btn-cart'>
+        Add to Cart
+        <i class='fa-solid fa-cart-plus'></i>
+      </button>
+    </div>
+  );
+};
+
+export default Product;
