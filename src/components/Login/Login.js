d88fbe95842f2d5409ed23a269b5b2f599c550e9
Showrove Roy <showrove200@gmail.com>
2022-10-24 01:10:43 +0600

    Error message is done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
+  const [errorMess, setErrorMess] = useState(null);
+
+  const loginHandel = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    if (password.length < 6) {
+      setErrorMess("Password length should be 6 characters");
+      return;
+    }
+    console.log(email, password);
+    form.reset();
+  };
   return (
     <div>
       <div className='login-page'>
         <div className='form'>
           <h2 className='headIng'>Login</h2>
-          <form className='login-form'>
+          <h5 className='errorMess'>{errorMess}</h5>
+
+          <form onSubmit={loginHandel} className='login-form'>
             <input type='email' placeholder='email' name='email' required />
             <input
               type='password'
@@ -16,7 +32,7 @@ const Login = () => {
               name='password'
               required
             />
-            <button>login</button>
+            <button type='submit'>login</button>
             <p className='message'>
               Not registered? <Link to='/signup'>Create an account</Link>
             </p>
--- src/components/Sign Up/SignUp.js
+++ src/components/Sign Up/SignUp.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [errorMess, setErrorMess] = useState(null);
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirmPassword = form.confirmPassword.value;
+    if (password.length < 6) {
+      setErrorMess("Password length should be 6 characters");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrorMess("Password doesn't Match");
+      return;
+    }
+    console.log(email, password, confirmPassword);
+
+    form.reset();
+  };
   return (
     <div>
       <div className='login-page'>
         <div className='form'>
           <h2 className='headIng'>Sign Up</h2>
-          <form className='register-form'>
+
+          <h5 className='errorMess'>{errorMess}</h5>
+          <form onSubmit={handelSubmit} className='register-form'>
             <input type='email' placeholder='email' name='email' required />
             <input
               type='password'
@@ -22,7 +44,7 @@ const SignUp = () => {
               name='confirmPassword'
               required
             />
-            <button>create</button>
+            <button type='submit'>create</button>
             <p className='message'>
               Already registered? <Link to='/login'>Sign In</Link>
             </p>
